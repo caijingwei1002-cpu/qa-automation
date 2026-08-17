dbe2ff34c9fdea1c730a313aaf53fd69bab4aee0
wink <caijingwei1002@gmail.com>
2026-08-17 11:21:48 +0800

    Complete Day 6 parameterized Todo tests

---
+++ test-projects/01-todomvc-ui/tests/test_todo_data.py
@@ -0,0 +1,17 @@
+import pytest
+from playwright.sync_api import Page, expect
+
+
+@pytest.mark.parametrize(
+    "todo_text",
+    ["Buy milk", "Learn pytest", "Write parameterized test"],
+)
+def test_create_todo_for_each_input(todo_page: Page, todo_text: str):
+    todo_input = todo_page.get_by_placeholder("What needs to be done?")
+    todo_items = todo_page.locator(".todo-list li")
+
+    todo_input.fill(todo_text)
+    todo_input.press("Enter")
+
+    expect(todo_items).to_have_count(1)
+    expect(todo_items.locator("label")).to_have_text(todo_text)
